3ddbc219b1e151dd2e4d60d9de1cc401997237a9
Rohan Nair <r@rohannair.ca>
2016-10-04 20:30:02 -0400

    Adding in hacky sql

---
+++ mission_users.joins.sql
@@ -0,0 +1,9 @@
+INSERT into missions_users (mission_id, user_id)
+values
+  (
+    'c2defaf1-e79c-4745-8796-f034f20561c4',
+    '9f392a5b-8fc7-48af-9e27-f9831e50aed7'
+  ),(
+    '1a42e500-306e-4b52-862b-953806f6631d',
+    '87eb4549-07c9-418c-8417-38af1c7a5805'
+  );
