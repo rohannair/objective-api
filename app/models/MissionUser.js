3c330e6abe83a62447a7793b83caf65e24a95ae2
Rohan Nair <r@rohannair.ca>
2016-10-18 16:04:49 -0400

    Fixing naming in MissionUser model

--- app/models/MissionUser.js
+++ app/models/MissionUser.js
@@ -1,10 +1,10 @@
 const { Model } = require('objection');
 const BaseModel = require('./Base');
 
-class Mission_Target extends BaseModel {
+class Mission_User extends BaseModel {
   static get tableName() {
     return 'missions_users';
   }
 }
 
-module.exports = Mission_Target;
+module.exports = Mission_User;
