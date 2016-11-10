e0fb70c8f9993786aaa3a2e20dfad5877f33f6fa
Rohan Nair <r@rohannair.ca>
2016-11-10 15:03:55 -0500

    Fixing a dual declaration bug

--- app/routes/v1/emails.js
+++ app/routes/v1/emails.js
@@ -6,14 +6,13 @@ import { sendEmail } from '../../config/mailer';
 
 const emailControllers = () => ({
   inviteUser: async (email) => {
-    const email = {
+    const template = {
       content: {
         from: {
           name: 'Quartermaster',
           email: 'noreply@qrtrmstr.com'
         },
         subject: 'You have been invited to Quartermaster!'
-        html: `<html><p>Hi there, you have been invited to Quartermaster by someone on your team!</p><p>You can sign up here: <a href="https://qrtrmstr.io/signup?token=${token}&email=${email}">https://qrtrmstr.io/signup</a></p></html>`
       },
       recipients: [
         { address: email }
