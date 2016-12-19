3929df2428612e7c51b6f66febd278fb5d99354a
Rohan Nair <r@rohannair.ca>
2016-12-19 11:49:35 -0500

    Moving auth sync data into app_metadata
    As outlined here:
    https://auth0.com/forum/t/setting-userids-via-management-api/4563/4

--- app/routes/public/users.js
+++ app/routes/public/users.js
@@ -28,98 +28,15 @@ const userControllers = User => ({
   },
 
   forgotPassword: async ctx => {
-    const { email } = ctx.request.body;
-    const domain = email.split('@')[1];
-    const token = await createRandomToken();
-
-    const company = Company.query()
-      .select()
-      .where({ domain });
-
-    if (!company) {
-      return ctx.throw(400, 'Invalid email domain; company not found');
-    }
-
-    const user = await User.query()
-      .update({
-        signup_token: token
-      })
-      .where({ email: email.toLowerCase() });
-
-    if (!user) {
-      return ctx.throw(400, 'User not found');
-    }
-
-    const forgotPasswordEmail = await forgotPassword({email, domain, token});
-    debug('EMAIL STATUS', forgotPasswordEmail);
-
-    ctx.status = 200;
-    ctx.body = {
-      message: `Password reset email sent to ${email}`
-    };
+    ctx.throw(500, 'DEPRECATED ENDPOINT');
   },
 
   finishInvite: async ctx => {
-    const { email, token, firstName, lastName, img, password } = ctx.request.body;
-
-    const digest = await encryptPassword(password);
-    const user = await User
-      .query()
-      .update({
-        email,
-        firstName,
-        lastName,
-        img: img || '',
-        pending: false,
-        digest,
-        signup_token: null
-      })
-      .whereNotNull('signup_token')
-      .andWhere('email', email)
-      .andWhere('signup_token', token)
-      .returning(['id', 'email', 'digest', 'role', 'company_id'])
-      .first();
-
-    if (!user) {
-      ctx.throw('Invalid signup_token', 400);
-    }
-
-    const loginToken = await genToken(user);
-
-    ctx.status = 200;
-    ctx.body = {
-      status: 0,
-      user: user.id,
-      token: loginToken,
-      companyId: user.companyId,
-      message: 'User details updated'
-    };
+    ctx.throw(500, 'DEPRECATED ENDPOINT');
   },
 
   signup: async ctx => {
-    const { body } = ctx.request;
-
-    const domain = body.email.split('@')[1];
-    const company = await Company
-      .query()
-      .where('domain', domain)
-      .select('id')
-      .first();
-
-    const newUser = {
-      ...addId({ email: body.email }),
-      digest: await encryptPassword(body.password),
-      company_id: company.id
-    };
-
-    const user = await User
-      .query()
-      .insert(newUser)
-      .where('company_id', company)
-      .returning(['id', 'first_name', 'last_name', 'email', 'role', 'img']);
-
-    ctx.status = 201;
-    ctx.body = { user: omit(user, 'digest') };
+    ctx.throw(500, 'DEPRECATED ENDPOINT');
   },
 
   createUser: async ctx => {
@@ -160,7 +77,7 @@ const userControllers = User => ({
     }
 
     await auth0.users.update({ id: user_id }, {
-      user_metadata: {
+      app_metadata: {
         oiq_id: user.id,
         c_id: user.companyId,
         role: user.role
