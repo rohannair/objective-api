b9200c2ab2ea0640d08d27ea18180a6bc2aa0217
David Mills <dave@1morepx.com>
2017-02-01 16:21:46 -0500

    add Collaborator mutation should be an await on the DB

--- app/queries/collaborators.js
+++ app/queries/collaborators.js
@@ -1,6 +1,6 @@
 
 export const addCollaborator = async (db, user, objective) => {
-  db('objectives_users')
+  return await db('objectives_users')
     .insert({
       objective_id: objective,
       user_id: user
